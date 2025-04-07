e9ef1737e0ab793244cc285398fde0a83d81f10c
Afaq Tahir <the.afaq.tech@gmail.comgit>
2025-04-07 10:48:25 +0100

    Fix: bind to 0.0.0.0 for Render

--- app.py
+++ app.py
@@ -181,5 +181,8 @@ def ocr_test():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
